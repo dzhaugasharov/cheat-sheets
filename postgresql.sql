b975e7177a04961049325761354596824cf714d0
Darkhan <darkhan.zhaugasharov@gmail.com>
2021-09-30 10:28:55 +0600

    Update postgresql.sql

--- postgresql.sql
+++ postgresql.sql
@@ -1,10 +1,16 @@
--- creating
-CREATE TABLE clients (
+-- DDL (Data Definition Language)
+
+CREATE TABLE example_table (
   id SERIAL PRIMARY KEY, 
   name character varying(200) NOT NULL, 
   phone character varying(30) NOT NULL DEFAULT "no", 
   email character varying(200) NOT NULL,
-  company_id integer NOT NULL REFERENCES companies (id)
+  company_id integer NOT NULL REFERENCES companies (id),
+  status character varying(100) NOT NULL CHECK (
+    status IN (
+      'done', 'in progress', 'delivery'
+    )
+  )
 );
 
 CREATE TABLE apples (
@@ -35,7 +41,6 @@ ALTER TYPE enum_name ADD VALUE 'new_values';
 ALTER TYPE enum_type ADD VALUE 'new_value' BEFORE 'old_value';
 ALTER TYPE enum_type ADD VALUE 'new_value' AFTER 'old_value';
 
-
 -- 1. rename the enum type you want to change
 alter type some_enum_type rename to _some_enum_type;
 -- 2. create new type
@@ -49,3 +54,21 @@ update some_table set some_column = _some_column::text::some_enum_type;
 -- 6. remove old column and type
 alter table some_table drop column _some_column;
 drop type _some_enum_type;
+
+
+
+
+-- DML (Data Manipulation Language)
+
+-- UPSERT query
+INSERT INTO clients (id, name, phone, email) 
+VALUES (9, 'Nelly Kelly', '77545454545', 'belly@mail.com') 
+ON CONFLICT (id) DO UPDATE 
+SET 
+  name = EXCLUDED.name, 
+  phone = EXCLUDED.phone, 
+  email = EXCLUDED.email;
+
+-- ON CONFLICT DO NOTHING
+INSERT INTO products (id, name, price, category) 
+VALUES (1, 'Shampoo 2', 1200, 'cosmetic') ON CONFLICT DO NOTHING;
