918443580e74dd0183d73f071444627797a581e0
Darkhan <darkhan.zhaugasharov@gmail.com>
2021-10-21 11:04:33 +0600

    Create maven.cmd

---
+++ maven.cmd
@@ -0,0 +1,8 @@
+# show super-pom + pom.xml redefination results
+mvn help:effective-pom
+
+# created project from archetype
+mvn archetype:create-from-project
+
+# cleans and installs all dependencies
+mvn clean install
